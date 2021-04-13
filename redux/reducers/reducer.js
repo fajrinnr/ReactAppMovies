9c58779acaad91cd59b6144ab79aef7afd91d681
fajrinnr <fajrin.noorrachman11@gmail.com>
2021-04-13 09:53:28 +0700

    Updated Redux - Rename reducer and action - Add some new state - Add some new actions

---
+++ redux/reducers/reducer.js
@@ -0,0 +1,64 @@
+const initialState = {
+  recommendations: [],
+  movies: {
+    page: 1,
+    data: [],
+  },
+  series: {
+    page: 1,
+    data: [],
+  },
+  episodes: {
+    page: 1,
+    data: [],
+  },
+};
+
+const teamReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_RECOMMENDATION":
+      // let allFavTeam = state.favoriteTeam.concat(action.payload.teams)
+      return { ...state, recommendations: action.payload.recommendation };
+    case "ADD_MOVIES":
+      let allMovies = {
+        page: action.payload.page,
+        data: state.movies.data.concat(action.payload.movies),
+      };
+      return { ...state, movies: allMovies };
+    case "ADD_SERIES":
+      let allSeries = {
+        page: action.payload.page,
+        data: state.series.data.concat(action.payload.series),
+      };
+
+      return { ...state, series: allSeries };
+    case "ADD_EPISODES":
+      let allEpisodes = {
+        page: action.payload.page,
+        data: state.episodes.data.concat(action.payload.episodes),
+      };
+      return { ...state, episodes: allEpisodes };
+    //   case 'UNFAV':
+    //     Swal.fire({
+    //       position: 'center',
+    //       icon: 'success',
+    //       title: `Successfully remove ${action.payload.team.name} from Favorite`,
+    //       showConfirmButton: false,
+    //       timer: 1000
+    //     })
+    //     let unFavTeam = state.favoriteTeam.filter(team => {
+    //       return team.team_id !== action.payload.team.team_id
+    //     })
+    //     return {...state, favoriteTeam: unFavTeam}
+    //   case 'GET_TEAM':
+    //     return {...state, allTeam: action.payload.team}
+    //   case 'SORT_ASC':
+    //     return {...state, allTeam: action.payload.teams}
+    //   case 'SORT_DESC':
+    //     return {...state, allTeam: action.payload.teams}
+    default:
+      return state;
+  }
+};
+
+export default teamReducer;
--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from "./reducers/recommendationReducer";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/reducer";
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, applyMiddleware(thunk));
 
-export default store
+export default store;
